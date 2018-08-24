97e76baa88eff4db203981c5d98870fe2f5a6e1e
zjm <zjm1060@163.com>
2018-08-24 12:55:17 +0800

    init commit

---
+++ contrib/scripts/post_build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+echo "***************************************"
+echo "*   target: ${TARGET_DIR}"
+echo "*   external: ${BR2_EXTERNAL}"
+echo "*   hostdir: ${HOST_DIR}" 
+echo "*   output: ${BASE_DIR}"
+echo "***************************************"
